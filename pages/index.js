421dc5772db472ab055221f67d1e22f71840589c
HannanZikan <hannanczikan@gmail.com>
2026-04-21 20:47:07 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Lívia, eu te amo e quero me casar e ter vários filhos com você!</h1>
+  return (
+    <h1>Lívia, eu te amo e quero me casar e ter vários filhos com você!</h1>
+  );
 }
 
 export default Home;
